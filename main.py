b92d74dc1e2fcc6e81b605b284fb855995f56900
kk3k02 <kubajak@outlook.com>
2023-10-14 14:42:32 +0200

    main update - read from file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world!")
+import readFile
+
+#Ścieżka do pliku z danymi
+filePath = "dane.txt"
+
+def main():
+    #Wczytanie macierzy sąsiedztwa
+    file = readFile.ReadFile(filePath)
+    data = file.readData()
+    print(data)
+
+#Start programu
+if __name__ == "__main__":
+    main()
