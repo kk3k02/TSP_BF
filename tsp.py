2a626ff429a138462cd272613ad335da2ab40fb5
kk3k02 <kubajak@outlook.com>
2023-10-16 12:45:55 +0200

    some optimalization

--- tsp.py
+++ tsp.py
@@ -3,34 +3,37 @@ import itertools
 
 class Tsp:
     def __init__(self, n, cost):
-        self.n = n
-        self.cost = cost
+        self.n = n  # Liczba punktów docelowych (miast)
+        self.cost = cost  # Macierz kosztów odległości pomiędzy miastami
 
+    # Metoda obliczająca całkowitą odległość trasy
     def calculate_total_distance(self, path):
         total_distance = 0
         for i in range(self.n - 1):
             start_node = path[i]
             end_node = path[i + 1]
             total_distance += self.cost[start_node][end_node]
-        total_distance += self.cost[path[-1]][path[0]]
+        total_distance += self.cost[path[-1]][path[0]]  # Zamykanie trasy
         return total_distance
 
+    # Metoda rozpoczynająca rozwiązywanie problemu
     def start(self):
         if self.n <= 2:
-            return list(range(self.n)), 0
+            return list(range(self.n)), 0  # Obsługa przypadków z mniej niż trzema punktami
 
-        vertices = list(range(self.n))
+        vertices = list(range(self.n))  # Lista punktów docelowych
 
-        min_dist = float('inf')
-        best_path = None
+        min_dist = float('inf')  # Początkowo zakładamy nieskończoną odległość
+        best_path = None  # Początkowo brak najlepszej trasy
 
+        # Przeszukiwanie wszystkich permutacji punktów docelowych
         for path in itertools.permutations(vertices):
             total_dist = self.calculate_total_distance(path)
             if total_dist < min_dist:
                 min_dist = total_dist
                 best_path = path
 
-        best_path = list(best_path)  # Konwertuj na listę tylko na końcu
-        best_path = best_path + [best_path[0]]  # Dodaj wierzchołek startowy na koniec trasy
+        best_path = list(best_path)
+        best_path = best_path + [best_path[0]]  # Dodanie wierzchołka startowego na koniec trasy
 
-        return int(min_dist), best_path
+        return int(min_dist), best_path  # Zwracanie najlepszej trasy i kosztu drogi
